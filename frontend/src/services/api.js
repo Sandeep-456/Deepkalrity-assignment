31dcf91d45481993bdbdad49522d58904c435835
Sandeep-456 <sandeepindugula456@gmail.com>
2025-09-07 00:20:06 +0530

    added the urls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = 'https://deepkalrity-assignment-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
